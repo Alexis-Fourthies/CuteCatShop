766b941ad168a7a21cefa33dde45f0b1b1ce5588
Alexis-Fourthies <alexisfourthies@gmail.com>
2022-06-08 18:40:30 +0200

    add create_cart to UserModel + Cart link to navbar & Add to cart link to visitors

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
 
   has_one :cart
   has_many :orders
+
+  after_create :create_cart
+
+  def create_cart
+    puts "Creating cart"
+    Cart.create(user_id: User.find_by(email: email).id)
+  end
 end
