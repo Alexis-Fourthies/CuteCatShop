419b7d8b05a685f09a48c86f5bb4c9c788f18c26
Alexis Fourthies <103060337+Alexis-Fourthies@users.noreply.github.com>
2022-06-08 09:42:18 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,6 @@ Rails.application.routes.draw do
 
   resources :items
 
-  resources :carts do
-    resources :items
-  end
-
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
