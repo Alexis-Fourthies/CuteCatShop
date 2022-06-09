b386f5cc70599126c26eb91dea33344873e7d541
Julien Armagnac <julien.tamil@gmail.com>
2022-06-09 10:46:36 +0200

    Update orders#controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,10 +11,12 @@ class OrdersController < ApplicationController
     # Affichage de contrôle de fonction (supprimable à terme)
     puts "#"*100
     puts "On est arrivé dans orders#create"
+    puts "params[:total] = #{params[:total]}"
+    puts "params[:total].class = #{params[:total].class}"
     puts "#"*100
 
     # Transformer le panier en nouvelle commande
-    @order = Order.create(user:current_user, total_amount:params[:total]) #Le total_amount sera mettre à jour après le travail sur le back du carts#show
+    @order = Order.create(user:current_user, total_amount:params[:total].to_f) #Le total_amount sera mettre à jour après le travail sur le back du carts#show
     
     # Vérififcation de la création avec des puts en console
     puts "#"*100
@@ -28,10 +30,9 @@ class OrdersController < ApplicationController
         
         cart_item.destroy
         flash.notice = "Cart vidé (via une commande cart_item.destroy)"
-        redirect_to root_path and return
-      else
+      end
     end
-
+    redirect_to root_path
   end
 
   def show
