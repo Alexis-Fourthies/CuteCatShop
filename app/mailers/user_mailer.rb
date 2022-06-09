df1cfebbe52fd8d21ab281a1e5830dc9a1d8fc02
Julien Armagnac <julien.tamil@gmail.com>
2022-06-09 19:52:59 +0200

    Update Mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,10 @@
 class UserMailer < ApplicationMailer
-<<<<<<< HEAD
 
-  # default from: 'n.cubilier@gmail.com' # à ne pas modifier car c'est la seule 'sender address' validée sur mon compte mailJet pour le moment.
-  default from: 'cutecatshop@julien-armagnac.com'
-
-=======
   # default from: 'n.cubilier@gmail.com' # MAILJET
   # default from: 'cutecatshop@julien-armagnac.com' #SENDGRID
   default from: 'potatosaurusflex@ik.me' # MAILJET
   
->>>>>>> sendgrid_settings
+
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user 
