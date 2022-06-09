f441373b270d37dc4a30248a935b0a21cfc1754c
0xKubitus <n.cubilier@gmail.com>
2022-06-09 11:06:54 +0200

    UserMailer configurated + views welcome_email created

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,14 @@
 class UserMailer < ApplicationMailer
+  default from: 'n.cubilier@gmail.com'
+ 
+  def welcome_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'https://cutecatshop.herokuapp.com/' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Bienvenue chez CuteCatShop.fr !') 
+  end
 end
