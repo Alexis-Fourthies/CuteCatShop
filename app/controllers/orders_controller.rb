06bc0d6d95e82f88b2179e18334cadb45cd9fc11
0xKubitus <n.cubilier@gmail.com>
2022-06-09 11:02:48 +0200

    UserMailer generated

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,11 +11,13 @@ class OrdersController < ApplicationController
     # Affichage de contrôle de fonction (supprimable à terme)
     puts "#"*100
     puts "On est arrivé dans orders#create"
+    puts "params[:total] = #{params[:total]}"
+    puts "params[:total].class = #{params[:total].class}"
     puts "#"*100
 
     # Transformer le panier en nouvelle commande
-    @order = Order.create(user:current_user, total_amount:params[:total]) #Le total_amount sera mettre à jour après le travail sur le back du carts#show
-    
+    @order = Order.create(user:current_user, total_amount:params[:total].to_f) #Le total_amount sera mettre à jour après le travail sur le back du carts#show
+
     # Vérififcation de la création avec des puts en console
     puts "#"*100
     puts "La commande nouvellement créée est :\n#{@order.inspect}"
@@ -25,13 +27,12 @@ class OrdersController < ApplicationController
     all_cart_items = CartItem.all
     all_cart_items.each do |cart_item|
       if cart_item.cart == current_user.cart
-        
+
         cart_item.destroy
         flash.notice = "Cart vidé (via une commande cart_item.destroy)"
-        redirect_to root_path and return
-      else
+      end
     end
-
+    redirect_to root_path
   end
 
   def show
