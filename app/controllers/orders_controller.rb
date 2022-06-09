5ec02ad113f0f73e301ef604713a3808fddeddba
Julien Armagnac <julien.tamil@gmail.com>
2022-06-09 19:45:12 +0200

    Mailer can sends on yopmail (local env)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
-  
+
+
   def index
   end
   
@@ -59,5 +60,7 @@ class OrdersController < ApplicationController
 
   def destroy
   end
+
+
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,23 +4,32 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['MAILJET_LOGIN'],
+  :password => ENV['MAILJET_PWD'],
+  :domain => 'cutecatshop.fr',
+  :address => 'in-v3.mailjet.com',
+  :tls => true,
+  :port => '465',
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # ActionMailer::Base.smtp_settings = {
-#   :user_name => ENV['MAILJET_LOGIN'],
-#   :password => ENV['MAILJET_PWD'],
-#   :domain => 'cutecatshop.fr',
-#   :address => 'in-v3.mailjet.com',
-#   :tls => true,
-#   :port => '465',
+#   :user_name => 'apikey',
+#   :password => ENV['SENDGRID_PWD'],
+#   :domain => 'monsite.fr',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
 #   :authentication => :plain,
 #   :enable_starttls_auto => true
 # }
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey',
-  :password => ENV['SENDGRID_PWD'],
-  :domain => 'monsite.fr',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings =   {
+#     :address            => 'smtp.gmail.com',
+#     :port               => 587,
+#     :domain             => 'gmail.com', #you can also use google.com
+#     :authentication     => :plain,
+#     :user_name          => ENV['GMAIL_LOGIN'],
+#     :password           => ENV['GMAIL_PWD']
+#   }
