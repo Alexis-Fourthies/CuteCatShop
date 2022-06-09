3dce0e8b440f0f817f9596cb5e7db239bfcae75d
Julien Armagnac <julien.tamil@gmail.com>
2022-06-09 16:20:23 +0200

    Sendgrid ok locally.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,7 @@ class OrdersController < ApplicationController
   
   def new
     @order = Order.create(user:current_user, total_amount:params[:total].to_f)
+    
     @amount = @order.total_amount
     @stripe_amount = @amount*100
   end
@@ -14,7 +15,6 @@ class OrdersController < ApplicationController
     # Before the rescue, at the beginning of the method
     @order = Order.where(user:current_user).last
     @stripe_amount = (@order.total_amount*100).to_i # LIGNE D ORIGINE 500
-
     puts "#"*100
     puts "@stripe_amount = #{@stripe_amount}"
     puts "#"*100
@@ -40,10 +40,9 @@ class OrdersController < ApplicationController
     all_cart_items = CartItem.all
     all_cart_items.each do |cart_item|
       if cart_item.cart == current_user.cart
+        
         cart_item.destroy
-
         flash.notice = "paiement terminé - cart vidé (via une commande cart_item.destroy)"
-
       end
     end
     redirect_to root_path
